d9bb50721cdba9d7d92032f442917a6f1e78d69c
Lindsay Clark <lvclark@illinois.edu>
2021-12-13 13:39:57 -0600

    Add "unknown" to words considered missing data

--- R/helpers.R
+++ R/helpers.R
@@ -1,5 +1,5 @@
 findnonmissing <- function(x){
   !is.na(x) &
-    !grepl("(unclassified|uncultured|metagenome|unidentified)", x, ignore.case = TRUE) &
+    !grepl("(unclassified|uncultured|metagenome|unidentified|unknown)", x, ignore.case = TRUE) &
     x != "human_gut" & x != ""
 }
