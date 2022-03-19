f288d92e82f33d19dd375415417ea642f454d7f9
Lindsay Clark <lvclark@illinois.edu>
2022-03-19 11:27:01 -0500

    make_taxa_labels function

--- R/helpers.R
+++ R/helpers.R
@@ -3,3 +3,16 @@ findnonmissing <- function(x){
     !grepl("(unclassified|uncultured|metagenome|unidentified|unknown)", x, ignore.case = TRUE) &
     x != "human_gut" & x != ""
 }
+
+make_taxa_labels <- function(taxtab){
+  species_temp <- taxtab[,"Species"]
+  species_temp[!findnonmissing(species_temp)] <- "sp."
+  out <- character(nrow(taxtab))
+  for(rnk in colnames(taxtab)[-ncol(taxtab)]){
+    nm <- findnonmissing(taxtab[,rnk])
+    out[nm] <- taxtab[nm,rnk]
+  }
+  out <- paste(out, species_temp)
+  names(out) <- rownames(taxtab)
+  return(out)
+}
